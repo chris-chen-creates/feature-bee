5bbe10d320844ace9545d1e667426fec38c8ddc2
Brian Dillmann <dillmann.brian@gmail.com>
2021-04-09 11:35:37 -0400

    update test so it needs to be fixed

--- server/src/api/feature/__tests__/feature.test.ts
+++ server/src/api/feature/__tests__/feature.test.ts
@@ -4,27 +4,24 @@ import Config from '../../../config';
 import FeatureController from '../controller';
 import FeatureDAO from '../model';
 
-describe('feature integration tests', async () => {
-  const config = Config.readFromEnvironment();
-  const db = mysql.createConnection(config.dbOptions());
-  const controller = new FeatureController(new FeatureDAO(db));
+const config = Config.readFromEnvironment();
+const db = mysql.createConnection(config.dbOptions());
+const controller = new FeatureController(new FeatureDAO(db));
 
-  test('create feature works', async () => {
-
-    await controller.createFeature({
-      name: 'test',
-      active: false,
-    });
-
-    const features = await controller.listFeatures();
-    expect(features).toEqual([{
-      id: 1,
-      name: 'test',
-      active: 0, // booleans get converted to integers
-    }]);
+test('create feature works', async () => {
+  const featureId = await controller.createFeature({
+    name: 'test',
+    active: false,
   });
 
-  afterAll(() => {
-    db.end();
-  });
+  const features = await controller.listFeatures();
+  expect(features).toEqual([{
+    id: featureId,
+    name: 'test',
+    active: 0, // booleans get converted to integers
+  }]);
+});
+
+afterAll(() => {
+  db.end();
 });
